f5a337bd93caf1c7e313dafd957cdd65be0248b9
Alona Chubenko <aliona.chubenko@gmail.com>
2024-02-02 13:01:22 +0200

    Connect Countries.jsx to store

--- src/stores/countriesSlice.js
+++ src/stores/countriesSlice.js
@@ -1,12 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
+import countriesService from "../services/countries";
+
+export const initializeCountries = () => {
+    return async (dispatch) => {
+        const countries = await countriesService.getAll();
+        dispatch(getCountries(countries)); //send countries(payload) to the reducer  reducers: {  getCountries(state, action)
+        setTimeout(() => {
+            dispatch(isLoading(false));
+        }, 1000);
+    };
+};
 
 export const countriesSlice = createSlice({
     name: "countries",
     initialState: {
-        countries: [],
-        isLoading: true,
+        countries: [], //to acces this: useSelector((state) => state.countries.countries)
+        isLoading: true, //to acces this: useSelector((state) => state.countries.isLoading)
     },
-    reducers: {},
-})
+    reducers: {
+        getCountries(state, action) {
+            state.countries = action.payload; //if it is success, then we update the state and isLoading to false
+            state.isLoading = false;
+        },
+        isLoading: (state, action) => {
+            state.isLoading = action.payload;
+        },
+    },
+});
+export const { getCountries } = countriesSlice.actions;
 
 export default countriesSlice.reducer;
--- src/stores/store.js
+++ src/stores/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import countriesReducer from "./countriesSlice";
 
 //store hosts slices (separete states)
+//store is state
 export default configureStore({
     reducer: {
         countries: countriesReducer,
