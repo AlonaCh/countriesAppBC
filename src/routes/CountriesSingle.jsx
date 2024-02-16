67709c9509193f48df0f9380793eb7110633f49b
Alona Chubenko <aliona.chubenko@gmail.com>
2024-02-16 11:55:07 +0200

    Add main body of the component

--- src/routes/CountriesSingle.jsx
+++ src/routes/CountriesSingle.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import {useLocation, useNavigate} from "react-router-dom";
 
 import { Container, Button, Col, Image, Row, Spinner} from "react-bootstrap";
+import { AltRoute } from "@mui/icons-material";
 
 const CountriesSingle = () => {
   //const {single} = useParams()
@@ -29,18 +30,52 @@ useEffect(() =>{
     setLoading(false);
   });
 },[country.capital]);
-
+console.log(weather);
 if(loading){
   return(
     <Col className="text-center m-5">
     <Spinner animation="grow" />
   </Col>
-  )
+  );
 }
 
   return (
     <Container>
-      <div>Single Country will be here</div>
+      <Row className="m-5">
+        <Col>
+        <Image
+thumbnail
+src={`https://source.unsplash.com/featured/1600x900?${country.name.common}`} alt={`${country.name.common}`}
+/>
+  </Col>
+  <Col>
+  <h2 className="display-4">{country.name.common}</h2>
+  <h3>{country.capital}</h3>
+  {!error && weather && (
+    <div>
+      <p>
+        Now it is <strong>{weather.main.temp}</strong>
+        degrees in {country.capital} and {weather.weather[0].description}
+      </p>
+      <p>Feels like: {weather.main.
+feels_like}</p>
+        <img
+        src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+        alt={weather.weather[0].description}
+        />
+      
+    </div>
+  )}
+  </Col>
+  </Row>
+      <Row>
+<Col>
+<Button variant="light" onClick={()=>navigate("/countries")}>
+
+</Button>
+</Col>
+</Row>
+     
     </Container>
   );
 };
