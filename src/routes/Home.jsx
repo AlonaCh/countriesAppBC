1181e5b51e0e6691fd65be5140574e35c38f8185
Alona Chubenko <aliona.chubenko@gmail.com>
2024-03-17 20:20:26 +0200

    Add footer

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -7,13 +7,13 @@ import { NavLink } from "react-router-dom";
 const Home = () => {
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen font-poppins">
+    <div className="flex flex-col justify-center items-center  font-poppins">
     
       <img className="object-cover w-screen mb-10" src={'world.jpg'} alt={`world`} />
     
       <h1 className="text-5xl underline decoration-yellow-300">Welcome to discover the world! </h1>
       <h3 className="text-2xl text-zinc-500 mt-3">Please start by logging</h3>
-      <Button className="border border-transparent bg-gray-900 text-white px-8 rounded-lg mt-6 mb-20 hover:bg-gray-500"> 
+      <Button className="border border-transparent bg-gray-900 text-white px-8 rounded-lg mt-6  hover:bg-gray-500"> 
       <NavLink to="/login" >Login</NavLink>
       </Button>
      
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function Root() {
     return (<div>
         <Header />
         <Outlet />
+        <Footer/>
     </div>);
 }
 
